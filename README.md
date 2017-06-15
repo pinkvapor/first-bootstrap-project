cc648fc604c78b37f24c7d1b419bf5cdb86395d9
ACE User <aceuser@e140-s04.lab.lagcc>
2017-06-14 21:16:10 -0400

    added random gif feature

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bootstrap Template
+
+Made a gif gen!
+
+
+
+This is a template using Bootstrap 3, remember to change github repo with
+
+git remote set-url origin _____________
+
+paste new link in for ___________
---
+++ js/script.js
@@ -0,0 +1,11 @@
+//var $mainheader = $('#main-header');
+//$mainHeadder.html('hello');
+
+$('#random-gif-btn').click( function(){
+$.get( "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg", function( data ) {
+	var img = '<img src=" ' + data.data.fixed_width_small_url + ' ">'
+ 	$('#random-gif-container').html(img);
+	});
+
+
+});
